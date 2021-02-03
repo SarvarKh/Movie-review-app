1891ddb49c4545723f80b9fbd6c362c4e6c16eb0
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-03 23:27:02 +0500

    Adding separate page for each movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,7 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+    def show
+        @movie = Movie.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/movies', to: 'movies#index'
+  get '/movies/:id', to: 'movies#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
