a209d3f0211669f7e38aa954cd9e249ef6a0cb24
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-04 00:20:06 +0500

    Link both pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root "movies#index"
   get '/movies', to: 'movies#index'
-  get '/movies/:id', to: 'movies#show'
+  get '/movies/:id', to: 'movies#show', as: 'movie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
