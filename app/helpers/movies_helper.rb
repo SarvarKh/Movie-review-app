d4881558e2d338b4e8a66056912b55ac02d9613c
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-03 22:53:53 +0500

    Using helpers to format data and use business logic

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def format_price(movie)
+        if movie.free?
+            content_tag(:strong, "Free!")
+        else
+            number_to_currency(movie.price)
+        end
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+    def free?
+        price == 0
+    end
 end
