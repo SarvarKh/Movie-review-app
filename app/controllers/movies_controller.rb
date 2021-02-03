a4a31a3ec268221b6d140ef0d02fca3d74b206ad
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-03 20:11:17 +0500

    Connect MVC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
     def index
-        @movies = ['Set your  Goals for 2021', 'You can do it!', 'Hard work and Discipline pays off']
+        @movies = Movie.all
     end
 end
