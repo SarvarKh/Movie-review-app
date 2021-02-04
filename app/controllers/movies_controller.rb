2be6bf80e2525682a956666a3d8d7c7c2c268b35
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-04 15:36:30 +0500

    Connect the form to update DB and reflect updated obj

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,4 +8,11 @@ class MoviesController < ApplicationController
     def edit
         @movie = Movie.find(params[:id])
     end
+    def update
+        @movie = Movie.find(params[:id])
+        movie_params = params.require(:movie).
+                        permit(:name, :description, :price, :location, :start_at)
+        @movie.update(movie_params)
+        redirect_to movie_path(@movie)
+    end
 end
