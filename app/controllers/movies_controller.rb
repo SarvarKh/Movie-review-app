bb3864fbad7575c2d563bd4348bb820474a27150
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-04 18:10:14 +0500

    Add destroy method to delete movie obj

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,24 +2,35 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
     def show
         @movie = Movie.find(params[:id])
     end
+
     def edit
         @movie = Movie.find(params[:id])
     end
+
     def update
         @movie = Movie.find(params[:id])
         @movie.update(movie_params)
         redirect_to @movie
     end
+
     def new
         @movie = Movie.new()
     end
+
     def create
         @movie = Movie.new(movie_params)
         @movie.save
         redirect_to @movie
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_url
     end
 end
 
