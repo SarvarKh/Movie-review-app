3984773e5c97dc0ce0e4fbda72942be490f21cfc
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-04 14:39:55 +0500

    Add edit form page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,7 @@ class MoviesController < ApplicationController
     def show
         @movie = Movie.find(params[:id])
     end
+    def edit
+        @movie = Movie.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "movies#index"
   get '/movies', to: 'movies#index'
   get '/movies/:id', to: 'movies#show', as: 'movie'
+  get '/movies/:id/edit', to: 'movies#edit', as: 'edit_movie'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
